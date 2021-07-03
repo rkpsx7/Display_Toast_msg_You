1dd0ccbbb6ac0950b244f2ffaed5c0a87359dc25
rkpsx7 <rkpsx7@gmail.com>
2021-07-03 11:31:52 +0530

    completed

--- app/src/main/java/com/example/display_toast_msg_you/MainActivity.java
+++ app/src/main/java/com/example/display_toast_msg_you/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.display_toast_msg_you;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBtnOne;
+    private Button mBtnTwo;
+    private Button mBtnThree;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnOne = findViewById(R.id.btnOne);
+        mBtnTwo = findViewById(R.id.btnTwo);
+        mBtnThree = findViewById(R.id.btnThree);
+
+        mBtnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Button One Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        mBtnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Button Two Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        mBtnThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Button Three Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
